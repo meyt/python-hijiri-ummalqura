d467f155d033b30c0726fe0a1199aaf832bfdd21
meytighg <meytighg@yahoo.com>
2015-03-17 01:33:50 +0330

    fix error on locale using

--- umalqurra/hijri_date.py
+++ umalqurra/hijri_date.py
@@ -7,7 +7,7 @@ This Api will give the ability to convert Gregorian To Hijri or Hijri to Gregori
 You can query for the current day in both Hijri and Gregorian
 '''
 __author__ = 'Khalid'
-from hijri import Umalqurra
+from umalqurra.hijri import Umalqurra
 from datetime import date
 class HijriDate:
     #day in hijri
@@ -23,10 +23,29 @@ class HijriDate:
     day_name = ''
     #month name in hijri
     month_name = ''
-    month_dict = {1:'محرم',2:'صفر',3:'ربيع الأول',4:'ربيع الثاني',5:'جمادي الأولى',6:'جمادي الآخرة',7:'رجب'
-    ,8:'شعبان',9:'رمضان',10:'شوال',11:'ذو القعدة',12:'ذو الحجة'}
-    day_dict = {'Saturday':'السبت','Sunday':'الاحد','Monday':'الاثنين','Tuesday':'الثلاثاء',
-                'Wednesday':'الاربعاء','Thursday':'الخميس','Friday':'الجمعة'}
+    month_dict = {
+        1: u'محرم',
+        2: u'صفر',
+        3: u'ربيع الأول',
+        4: u'ربيع الثاني',
+        5: u'جمادي الأولى',
+        6: u'جمادي الآخرة',
+        7: u'رجب',
+        8: u'شعبان',
+        9: u'رمضان',
+        10: u'شوال',
+        11: u'ذو القعدة',
+        12: u'ذو الحجة'
+    }
+    day_dict = {
+        '6': u'السبت',
+        '7': u'الاحد',
+        '1': u'الاثنين',
+        '2': u'الثلاثاء',
+        '3': u'الاربعاء',
+        '4': u'الخميس',
+        '5': u'الجمعة'
+    }
     month_name_gr = ''
     day_name_en = ''
     def __init__(self,year=None,month=None,day=None,gr=False):
@@ -42,7 +61,8 @@ class HijriDate:
         self.year, self.month, self.day = um.gegorean_to_hijri(year,month,day)
         self.month_name = self.month_dict[self.month]
         date_gr = date(year,month,day)
-        self.day_name_en = date_gr.strftime("%A")
+        self.day_name_en = date_gr.strftime("%u")
+        print self.day_name_en, date_gr.strftime("%a")
         self.day_name = self.day_dict[self.day_name_en]
         self.month_name_gr = date_gr.strftime("%B")
     #Set dates if date send by user is Hijri
@@ -52,7 +72,7 @@ class HijriDate:
         self.month_name = self.month_dict[month]
         self.year_gr, self.month_gr, self.day_gr = um.hijri_to_gregorian(year,month,day)
         date_gr = date(int(self.year_gr),int(self.month_gr),int(self.day_gr))
-        self.day_name_en = date_gr.strftime("%A")
+        self.day_name_en = date_gr.strftime("%u")
         self.day_name = self.day_dict[self.day_name_en]
         self.month_name_gr = date_gr.strftime("%B")
 
